d4bd09908e50c3ac49eff58f8f9ef0bd72f847a4
Aayush Patel <133498952+Aayush014@users.noreply.github.com>
2024-01-16 11:02:13 +0530

    Update 17 change_9_to_0.dart

--- Lecture 2-Lab Work/Logical Round/17 change_9_to_0.dart
+++ Lecture 2-Lab Work/Logical Round/17 change_9_to_0.dart
@@ -1,1 +1,11 @@
-
+import 'dart:io';
+void main() {
+  List num = [1, 2, 3, 9, 5, 7, 9, 0];
+  print(num);
+  for (int i = 0; i < num.length; i++) {
+    if (num[i] == 9) {
+      num[i] = 0;
+    }
+  }
+  print(num);
+}
