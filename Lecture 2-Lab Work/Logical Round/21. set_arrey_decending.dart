99357bda8fa1ceb57012f500c358d49a8ca31f3e
Aayush Patel <133498952+Aayush014@users.noreply.github.com>
2024-01-16 11:23:34 +0530

    Update 21. set_arrey_decending.dart

--- Lecture 2-Lab Work/Logical Round/21. set_arrey_decending.dart
+++ Lecture 2-Lab Work/Logical Round/21. set_arrey_decending.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
+
 void main() {
   List num = [3, 5, 1, 4, 2];
-  num.sort();
-  print("Ascending Order:-$num");
+  num.sort((a, b) => b.compareTo(a));
+  print("Descending Order:-$num");
 }
