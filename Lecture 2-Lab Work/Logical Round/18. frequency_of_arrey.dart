18603bc674aea13db76a187cc4e55bdc06c16d95
Aayush Patel <133498952+Aayush014@users.noreply.github.com>
2024-01-16 11:10:56 +0530

    Update 18. frequency_of_arrey.dart

--- Lecture 2-Lab Work/Logical Round/18. frequency_of_arrey.dart
+++ Lecture 2-Lab Work/Logical Round/18. frequency_of_arrey.dart
@@ -1,1 +1,19 @@
+import 'dart:io';
 
+void main() {
+  List num = [2, 3, 1, 6, 4, 1, 3, 1, 6, 7, 2];
+  Map<int, int> freq = {};
+
+  for (int i = 0; i < num.length; i++) {
+    int n = num[i];
+    if (freq.containsKey(n)) {
+      freq[n] = freq[n]! + 1;
+    } else {
+      freq[n] = 1;
+    }
+  }
+  print("Frequencies:-");
+  freq.forEach((key, value) {
+    print("$key:$value");
+  });
+}
